cd919cee166907ebb291615bab651f601ce0a38a
DiogoASTeixeira <diogo_alex@hotmail.com>
2018-10-31 13:06:35 +0000

    Typo in register.php

--- authentication/register.php
+++ authentication/register.php
@@ -4,9 +4,9 @@ include_once(__DIR__ . '/../templates/common/header.php');
 
 <form action='../actions/action_register.php' method="post">
 <label for="uname"><b>Username</b></label>
-<input type="text" name="username" required><br>
+<input type="username" name="username" required><br>
 
-<label for="mail"><b>Password</b></label>
+<label for="mail"><b>E-mail</b></label>
 <input type="email" name="email" required><br>
 
 <label for="psw"><b>Password</b></label>
