8da13cca6f579b142d2a07b018cff6e35130bd4b
DiogoASTeixeira <diogo_alex@hotmail.com>
2018-11-02 12:59:39 +0000

    Organisation. Started working on post page.

--- post/post_item.php
+++ post/post_item.php
@@ -1,5 +1,5 @@
 <?php
 include_once('../templates/common/header.php');
-include_once('../templates/posts/create_post.php');
+include_once('../templates/posts/post_item.php');
 include_once('../templates/common/footer.php');
 ?>
--- templates/posts/list_posts.php
+++ templates/posts/list_posts.php
@@ -4,7 +4,7 @@ include_once('../database/posts.php');
 $posts = getAllPosts();
 foreach ($posts as $post) {
 	?>
-	<h1><a href="post_item.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a></h1>
+	<h1><a href="../post/post_item.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a></h1>
 	<p id="author">by <a href="user_page.php?username=<?=$post['author']?>"><?=$post['author']?></a>
 	 in <?=date("d-m-Y", $post['date'])?></p>
 	<p id="textbody"><?= $post['textbody'] ?></p>
