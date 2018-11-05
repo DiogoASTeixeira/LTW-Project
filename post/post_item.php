cf5c6bc94daa8599034ec4f0bbf229be3766045c
DiogoASTeixeira <diogo_alex@hotmail.com>
2018-11-05 15:11:43 +0000

    Comments will be postponed until AJAX.

--- post/post_item.php
+++ post/post_item.php
@@ -1,6 +1,6 @@
 <?php
 include_once('../templates/common/header.php');
 include_once('../templates/posts/post_item.php');
-include_once('../templates/posts/post_comments.php');
+include_once('../templates/comments/post_comments.php');
 include_once('../templates/common/footer.php');
 ?>
---
+++ templates/comments/write_comment.php
@@ -0,0 +1,4 @@
+<form action="../actions/action_write_comment.php" method="post">
+    Comment: <input type="text" name="textbody" value=""><br>
+    <input type="submit">
+</form>
