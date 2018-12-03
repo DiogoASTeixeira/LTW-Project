96b939c514ca065d6e7639de6369c7b6876c73fa
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-12-03 15:35:19 +0000

    Access own profile only when logged in. To change later

--- profile/profile.php
+++ profile/profile.php
@@ -1,8 +1,12 @@
 <?php 
 include_once('../includes/session.php');
 include_once('../templates/common/header.php');
-
-include_once('../templates/profile/profile.php');
-
+if(isset($_SESSION['username']))
+    include_once('../templates/profile/profile.php');
+else{
+    //TODO must be logged in to access your profile
+    header("Location: ../index/index.php");
+}
+    
 include_once('../templates/common/footer.php');
 ?>
