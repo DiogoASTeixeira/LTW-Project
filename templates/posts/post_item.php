9a99294de532c013aee5b1e83ba17769b2990e9c
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-12-11 10:56:23 +0000

    Up/Down vote buttons, access self profile

--- templates/posts/post_item.php
+++ templates/posts/post_item.php
@@ -3,5 +3,7 @@
     <p id="author">by <a href="../profile/profile.php?username=<?= $post['username'] ?>"><?= $post['username'] ?></a></p>
     <p id="date">in <?= date("d-m-Y", $post['date']) ?></p>
     <p id="textbody"><?= $post['textbody'] ?></p>
+    <button type="button">Upvote</button> 
     <p id = upvote> <?=$post["upvotes"]?> </p>
+    <button type="button">Downvote</button> 
 </section>
