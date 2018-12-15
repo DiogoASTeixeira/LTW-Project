8ed1a6912c03b3be54f82c398fb9e6759b4eb7df
Meirinha <up201604350@fe.up.pt>
2018-12-15 11:50:37 +0000

    register is at least a bit pretty too

--- templates/authentication/register.php
+++ templates/authentication/register.php
@@ -1,7 +1,8 @@
+<img src="../images/quokka_login.png" alt="Quokka">
 <form action='../actions/action_register.php' method="post">
     <label for="uname"><b>Username</b></label>
     <input type="username" name="username" required><br>
-    
+
     <label for="psw"><b>Password</b></label>
     <input type="password" name="password" required><br>
 
